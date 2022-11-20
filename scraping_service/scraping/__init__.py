2553d85dc17b847448f7705b90e1527b00745568
ValShm <valboss@mail.ru>
2022-11-20 22:16:01 +0300

    third commit - upgrade vacancy and add utils.py

---
+++ scraping_service/scraping/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'scraping_service.scraping.apps.ScrappingConfig'
--- scraping_service/scraping/apps.py
+++ scraping_service/scraping/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ScrappingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'scraping'
+    verbose_name = 'Приложение по сбору вакансий'
